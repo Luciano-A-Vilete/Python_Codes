c5d6c8a28d321220e6e36adf4271cf94d72878b2
Luciano Armando Vilete <50681089+Luciano-A-Vilete@users.noreply.github.com>
2021-05-17 00:46:26 -0300

    Add files via upload
    A code for concatenate name and surname using python.

---
+++ BYU_codes/Vilete_002.py
@@ -0,0 +1,11 @@
+#Python Code 002
+
+#Asking the first name:
+first_name = input('What is your first name, please? ')
+
+#Asking the last name:
+last_name = input('What is your last name, please? ')
+
+#Printing the result:
+full_name_message = ('So, your name is: {}, {} {}.' .format(last_name, first_name, last_name))
+print(full_name_message)
