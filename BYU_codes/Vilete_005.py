99b5f197846027a051f231f00f1af0f8af98723a
Luciano Armando Vilete <50681089+Luciano-A-Vilete@users.noreply.github.com>
2021-05-25 07:27:18 -0300

    Playing with numbers.
    Playing with the basic operations and numbers.

---
+++ BYU_codes/Vilete_005.py
@@ -0,0 +1,15 @@
+#Python Code 005
+
+#Asking the age:
+
+age = int(input('How old are you? '))
+print(f'On your next brithday you will have {age + 1} years!')
+
+#Egg Cartons:
+cartons = int(input('How much cartons do you have? '))
+print(f'So, that means that you have {cartons * 12} eggs!')
+
+#People love cookies!!!
+cookies = float(input('How many cookies do you have? '))
+people = float(input('How many people are there? '))
+print(f'Each person may eat {cookies / people} cookies.')
